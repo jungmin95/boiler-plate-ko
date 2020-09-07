4e875cb3410944459a8f5b1d813f18096aa3c9c7
jungmin95 <rhemrgks12@gmail.com>
2020-09-07 22:36:56 +0900

    node.login

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const config = require('./config/key');
+const config = require('./config/key')
 
 const { User } = require("./models/User")
 
@@ -12,7 +13,8 @@ const { User } = require("./models/User")
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -34,6 +36,9 @@ app.post('/register', (req, res) => {
   
   //req.body안에 json형식으로 들어있음 왜? bodyparser를 쓰기때문
   const user = new User(req.body)
+  //save 전에 비밀번호 암호화
+
+  //save
 
   user.save((err, userInfo) => {
     if(err) return res.json({ success: false, err} )
@@ -42,6 +47,35 @@ app.post('/register', (req, res) => {
     })
   })
 
+})
+app.post('/login', (req, res) => {
+  
+  
+  //요청된 email을 데이터베이스에서 찾는다
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword( req.body.password, (err, isMatch) => {
+      if(!isMatch)
+      return res.json({ loginSuccess:false, message : "비밀번호가 틀렸습니다/"})
+      
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+        
+        //토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지 -> 쿠키
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess : true, userId: user._id})
+
+      })
+    })
+  })
+  //비밀번호 까지 맞다면 토큰을 생성하기
 })
 
 app.listen(port, () => {
