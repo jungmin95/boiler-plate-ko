dc976f0dc7f216f3c850fbf415c832251b92af2a
jungmin95 <rhemrgks12@gmail.com>
2020-09-08 15:59:43 +0900

    logout

--- models/User.js
+++ models/User.js
@@ -59,31 +59,46 @@ userSchema.pre('save', function( next ) {
 }
 })
 
-userSchema.methods.comparePassword = function(plainPassword, cb) {
-    //plainPassword 1234567  == 암호화된 비밀번호 $2b$10$Aj/6UtQ5NdFNFsvVUtBTS.b.Psk90E76.50fm2YUpvPWP6Hufqisy
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-        if(err) return cb(err),
-        cb(null, isMatch)
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
     })
 }
 
-userSchema.methods.generateToken = function(cb) {
-
+userSchema.methods.generateToken = function (cb) {
     var user = this;
+    
 
-    //jsonwebtocken을 이용해서 token을 생성
+    // jsonwebtoken을 이용해서 token을 생성하기 
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
-
-    // user._id + 'secretToken' = token
-
-    // ->
-
+    // user._id + 'secretToken' = token 
+    // -> 
     // 'secretToken' -> user._id
 
     user.token = token
-    user.save(function(err, user) {
-        if(err) return cb(err)
+    user.save(function (err, user) {
+        if (err) return cb(err)
         cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    var user = this;
+    
+    // user._id + '' = token
+    //토큰은 decode 한다. 
+
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에 
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id" : decoded, "token" : token}, function(err, user){
+
+            if(err) return cb(err);
+            cb(null, user) 
+        })
     })
 }
 
