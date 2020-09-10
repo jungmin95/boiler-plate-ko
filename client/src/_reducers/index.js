6c5657cbc971b4729abae274a3adace2107dd349
jungmin95 <rhemrgks12@gmail.com>
2020-09-10 21:09:05 +0900

    login register logout boiler-plate

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import user from './user_reducer';
-import commnet from './commnet_reducer';
+// import commnet from './commnet_reducer';
 
 const rootReducer = combineReducers({
-    // user,
+    user
     // commnet
 })
 
